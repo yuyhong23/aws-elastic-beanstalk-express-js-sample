a5f3dd60ccc19f7889098acb63aa7e62ac142e59
yuying <70069160+yuyhong23@users.nonreply.github.com>
2021-07-19 16:25:59 -0700

    Full pipeline test.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Today is Monday.'));
+app.get('/', (req, res) => res.send('Message changed.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
